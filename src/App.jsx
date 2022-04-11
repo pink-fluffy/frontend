20b3380259e8792c46beda61c58a7792cc962d40
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2022-04-11 13:46:00 -0400

    Added Splash Screen With Sources Made Svg Logo

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import logo from './logo.svg';
+import './App.css';
+
+const App =  () => {
+return  <div className="App">
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Currently Following <code>React E-Commerce App Design Tutorial</code>.
+        </p>
+        <a
+          className="App-link"
+          href="https://youtu.be/c1xTDSIXit8"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Video Link
+        </a>
+        <p>
+          For Middleware <code> React Storefront Page Example</code>.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/saleor/react-storefront"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Page Link
+        </a>
+      </header>
+    </div>
+
+};
+export default App;
